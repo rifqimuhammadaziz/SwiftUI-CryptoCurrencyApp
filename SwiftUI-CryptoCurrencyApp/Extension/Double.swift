8717b1eb5b76a89c404e353a5cda551087be19d6
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-02-21 21:20:17 +0700

    add percentage formatter & color condition

---
+++ SwiftUI-CryptoCurrencyApp/Extension/Double.swift
@@ -0,0 +1,38 @@
+//
+//  Double.swift
+//  SwiftUI-CryptoCurrencyApp
+//
+//  Created by Rifqi Muhammad Aziz on 21/02/23.
+//
+
+import Foundation
+
+extension Double {
+    // Set Currency to 2 Digit Number after Comma
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    // Set Percentage to 2 Digit Number after Comma
+    private var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? "$0.00"
+    }
+    
+    func toPercentString() -> String {
+        guard let numberAsString = numberFormatter.string(for: self) else { return "" }
+        return numberAsString + "%"
+    }
+}
